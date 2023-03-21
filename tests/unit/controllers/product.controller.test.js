2b9028170aab289dd3986f46496ea12816e6ce96
Melqui Brito <Merkulino11@gmail.com>
2023-03-21 20:31:24 -0300

    implementando mais alguns testes

--- tests/unit/controllers/product.controller.test.js
+++ tests/unit/controllers/product.controller.test.js
@@ -1,5 +1,8 @@
 const { expect } = require('chai');
+const chai = require('chai');
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 const { productsService } = require("../../../src/services");
 const { productsController } = require('../../../src/controllers');
 const { productsMockData } = require('../mocks/products.mock');
@@ -8,18 +11,17 @@ describe('Product Controller Test', () => {
   it('return product obj from id', async () => {
     const res = {};
     const req = {
-      params: {
-        id: 1,
-      },
+      params: { id: 1 },
     };
+
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
 
     sinon.stub(productsService, 'getById').resolves({ type: null, message: productsMockData[0] });
-
     await productsController.getById(req, res);
 
-    // expect(res.json).to.deep.equal(productsMockData[0]);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(productsMockData[0]);
   });
 
   it('returns 404 error when product id not exist', async () => {
@@ -36,9 +38,49 @@ describe('Product Controller Test', () => {
 
     await productsController.getById(req, res);
 
-    // expect(res.status).to.have.been.calledWith(404);
-    // expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
   });
+  
+  it('add new product', async () => {
+    const res = {};
+    const req = {
+      body: {
+        name: 'Traje de encolhimento',
+      },
+    };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productsService, 'newProduct').resolves({ type: null, message: productsMockData[1] });
+
+    await productsController.newProduct(req, res);
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(productsMockData[1]);
+  });
+  
+  it('validate the field name when add new product', async () => {
+    const res = {};
+    const req = {
+      body: {
+        name: 'TR',
+      },
+    };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productsService, 'newProduct').resolves({ type: null, message: productsMockData[1] });
+
+    await productsController.newProduct(req, res);
+    
+    // Não sei como testa a validação dos middlewares
+
+    // expect(res.status).to.have.been.calledWith();
+    // expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
+  });
+
+  
   afterEach(function () {
     sinon.restore();
   });

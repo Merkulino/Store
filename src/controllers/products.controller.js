d8d7f9ef803322b2bee59368589c069005b4c247
Melqui Brito <Merkulino11@gmail.com>
2023-03-21 16:50:23 -0300

    Fix: req 01

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -9,7 +9,7 @@ const getAll = async (req, res) => {
 const getById = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productsService.getById(id);
-  if (type) return res.status(404).json(message);
+  if (type) return res.status(404).json({ message });
   res.status(200).json(message);
 };
 

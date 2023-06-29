9b3ff99f5fa660a1d66cf9c4a80f3287e679bef9
Melqui Brito <Merkulino11@gmail.com>
2023-06-29 16:57:51 -0300

    Feat: Config connection migrate and seed mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const app = require('./app');
+const { seedDatabase } = require('./models/productsODM');
+const connectDB = require('./models/db.connection');
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
-
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Escutando na porta ${process.env.PORT || 3001}`);
+connectDB.then(() => {
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`Escutando na porta ${process.env.PORT || 3001}`);
+  });
+  seedDatabase();
+}).catch((error) => {
+  console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(0);
 });
